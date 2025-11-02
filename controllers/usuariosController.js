109a89e9d6eebd0fbb08409d58ec8701cffa80c8
MarcosFlores178 <marcosfabianflores@gmail.com>
2025-11-02 00:08:11 -0300

    vistas de perfil y edicion de perfil

--- controllers/usuariosController.js
+++ controllers/usuariosController.js
@@ -12,8 +12,38 @@ const usuariosController = {
             res.status(500).json({ error: "Error interno del servidor" });
         }
 
-       
+
+    },
+
+showProfile: async (req, res) => {
+    try {
+        const userId = req.session.userId;
+        const usuario = await Usuario.findByPk(userId);
+        if (!usuario) {
+            return res.status(404).send("Usuario no encontrado");
+        }
+
+        res.render('users/dataUser', { usuario });
+    } catch (error) {
+        console.error("Error al obtener el perfil del usuario:", error);
+        res.status(500).json({ error: "Error interno del servidor" });
     }
+},
+    editProfile: async (req, res) => {
+        const datos = req.body;
+
+        try {
+            const userId = req.session.userId;
+            const usuario = await Usuario.findByPk(userId);
+            if (!usuario) {
+                return res.status(404).send("Usuario no encontrado");
+            }
+            res.render('users/editProfile', { usuario });
+        } catch (error) {
+            console.error("Error al obtener el perfil del usuario para editar:", error);
+            res.status(500).json({ error: "Error interno del servidor" });
+        }
+}
 };
 
 module.exports = usuariosController;
--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,9 +1,13 @@
 let express = require('express');
+const {checkRole} = require('../middlewares/roleMiddleware');
 let router = express.Router();
+const usuariosController = require('../controllers/usuariosController');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+router.get('/profile', checkRole(['superadmin', 'organizador', 'administrativo']), usuariosController.showProfile);
 
 module.exports = router;
